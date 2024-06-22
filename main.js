2130566841fa538b7780b10ae0573445bb183528
Aiman <160181916+AimanUbaid@users.noreply.github.com>
2024-06-23 01:51:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("Hello World! from Aiman");
+//variable declaration
+var Name = "John Doe";
+var Age = 30;
+var Admin = true;
+console.log(Name);
+console.log(Age);
+console.log(Admin);
+//Concatenation
+var FirstName = "john";
+var LastName = "Doe";
+console.log("john" + "" + "Doe");
+var Greeting = "Hello";
+var Name2 = "JohnDoe";
+var message = Greeting + ' ' + Name2;
+console.log(message);
+//primitive data types
+var num = 10;
+var str = "hello";
+var bool = true;
+console.log(num);
+console.log(str);
+console.log(bool);
+//data types
+var person = {
+    name: "jon Doe",
+    age: 30,
+};
+console.log(person);
+//2"""
+var clr = ["red", "blue", "green"];
+console.log(clr[2]);
+//wit obj
+var adress = {
+    street: "123 Main ST",
+    city: "pelestine",
+    state: "ghaza"
+};
+console.log(adress.state);
---
+++ main.ts
@@ -0,0 +1,45 @@
+console.log("Hello World! from Aiman");
+
+//variable declaration
+let Name:String = "John Doe";
+let Age:Number = 30;
+let Admin:Boolean = true;
+
+console.log(Name);
+console.log(Age);
+console.log(Admin);
+
+//Concatenation
+const FirstName:string = "john";
+const LastName:string = "Doe";
+console.log("john" + "" + "Doe");
+
+let Greeting:string = "Hello";
+let Name2:string = "JohnDoe"
+let message:string = Greeting +' '+ Name2
+console.log(message)
+
+//primitive data types
+let num:number = 10
+let str:string = "hello"
+let bool:boolean = true
+console.log(num)
+console.log(str)
+console.log(bool)
+//data types
+
+let person = {
+    name: "jon Doe",
+    age: 30,
+}
+console.log(person)
+//2"""
+let clr = ["red", "blue", "green"]
+console.log(clr[2]);
+//wit obj
+let adress = {
+    street: "123 Main ST",
+    city: "pelestine",
+    state: "ghaza"
+};
+console.log(adress.state)
